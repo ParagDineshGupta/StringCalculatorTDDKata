4ad17621e14cbf8f8798e8b88653f70f62115457
ParagDineshGupta <guptaparag1996@gmail.com>
2025-03-12 12:19:21 +0530

    handle non numeric value

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,7 +1,16 @@
 export class StringCalculator {
-    add(numbers: string): number {
-        if (numbers === "") return 0;
-        const numArray = numbers.split(',')
-        return numArray.reduce((sum, num) => sum + +num, 0);
+  add(numbers: string): number | string {
+    try {
+      if (numbers === "") return 0;
+      const numArray = numbers.split(",");
+      const sum = numArray.reduce((sum, num) => sum + +num, 0);
+      return sum >= 0
+        ? sum
+        : "Invalid input. Please enter comma-separated numbers.";
+    } catch (error) {
+      console.error(error);
+      return "Invalid input. Please enter comma-separated numbers.";
+      // throw new Error("Invalid input. Please enter comma-separated numbers.");
     }
+  }
 }
