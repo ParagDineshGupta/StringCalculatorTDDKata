f03c866c95f3978e9ae7f5e0902fe74a7e3b768b
ParagDineshGupta <guptaparag1996@gmail.com>
2025-03-12 11:58:49 +0530

    jest RTL setup

---
+++ jest.setup.ts
@@ -0,0 +1,5 @@
+// Setup Jest and RTL
+// https://dev.to/teyim/effortless-testing-setup-for-react-with-vite-typescript-jest-and-react-testing-library-1c48
+// https://dev.to/ivadyhabimana/setup-jest-and-react-testing-library-in-a-react-project-a-step-by-step-guide-1mf0
+import "@testing-library/jest-dom";
+
