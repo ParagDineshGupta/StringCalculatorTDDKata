5f2ee31836e6641e9fdeea95216c4f8c4efaf38b
ParagDineshGupta <guptaparag1996@gmail.com>
2025-03-12 11:03:38 +0530

    create add method for string calc

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { StringCalculator } from './StringCalculator';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState(null)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="text" placeholder="Enter numbers to calculate" maxLength={10} />
+      <button onClick={() => {
+        const calculator = new StringCalculator()
+        const result = calculator.add(document.querySelector('input')?.value)
+        setResult(result)
+      }}>Calculate</button>
+      {!!result && <p>Result: {result}</p>}
     </>
   )
 }
